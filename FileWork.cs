a749ed786facb14afcf2cc352ffec94f68decb99
isaevDaniil <isaev.daniil48@yandex.ru>
2020-09-27 19:08:39 +0300

    Добавьте файлы проекта.

---
+++ FileWork.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProc
+{
+    static class FileWork
+    {
+        public static void ReadF(ref int N, ref int[] inputArr, ref int K)
+        {
+            try
+            {
+                using (var sr = new StreamReader("input.txt"))
+                {
+                    N = int.Parse(sr.ReadLine());
+                    inputArr = new int[N];
+                    var str = sr.ReadLine().Split(new char[] { ' ' });
+                    for (int i = 0; i < N; i++)
+                    {
+                        inputArr[i] = int.Parse(str[i]);//если во второй строке файла больше чем N символов,отсавшиеся не буду прочитаны
+                    }
+                    K = int.Parse(sr.ReadLine());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static void WriteF(int[] outputArr)
+        {
+            try
+            {
+                using (var sw = new StreamWriter("output.txt"))
+                {
+                    foreach (var item in outputArr)
+                    {
+                        sw.Write(item.ToString() + " ");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 0, K = 0;
+            int[] inputArr = new int[1];
+            FileWork.ReadF(ref N, ref inputArr, ref K);
+            int[] outputArr = new int[K];
+
+            PartialQuicksort(inputArr, 0, inputArr.Length - 1, K);
+            for (int i = 0; i < K; i++)
+            {
+                outputArr[i] = inputArr[i];
+            }
+            FileWork.WriteF(outputArr);
+        }
+        private static void PartialQuicksort(int[] input, int left, int right, int k)
+        {
+            while (right > left)
+            {
+                int pivot = Partition(input, left, right);
+                if (k - 1 <= pivot)
+                {
+                    right = pivot - 1;
+                }
+                else if (pivot - left > right - pivot)
+                {
+                    Quicksort(input, pivot + 1, right);
+                    right = pivot - 1;
+                }
+                else
+                {
+                    Quicksort(input, left, pivot - 1);
+                    left = pivot + 1;
+                }
+            }
+        }
+        private static void Quicksort(int[] input, int left, int right)
+        {
+            if (left >= right) return;
+
+            int pivot = Partition(input, left, right);
+            Quicksort(input, left, pivot - 1);
+            Quicksort(input, pivot + 1, right);
+
+        }
+
+        private static int Partition(int[] input, int left, int right)
+        {
+            var pointer = left;
+
+            for (int i = left; i <= right; i++)
+            {
+                if (input[i] > input[right])
+                {
+                    Swap(input, pointer, i);
+                    pointer++;
+                }
+            }
+            Swap(input, pointer, right);
+            return pointer;
+        }
+
+        private static void Swap(int[] ar, int a, int b)
+        {
+            int temp = ar[a];
+            ar[a] = ar[b];
+            ar[b] = temp;
+        }
+    }
+}
